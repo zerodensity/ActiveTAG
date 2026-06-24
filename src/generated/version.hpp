c96bc9dcce40804e4615d3b38fb6f6720a974394
Tolga Ergin <tergin76@users.noreply.github.com>
2026-06-24 14:46:31 +0300

    Release ActiveTAG Configurator v0.6.0

--- src/generated/version.hpp
+++ src/generated/version.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
 #define ACTIVETAG_VERSION_MAJOR 0
-#define ACTIVETAG_VERSION_MINOR 5
-#define ACTIVETAG_VERSION_PATCH 10
-#define ACTIVETAG_VERSION_W L"v0.5.10"
-#define ACTIVETAG_VERSION_A "v0.5.10"
-#define ACTIVETAG_APP_TITLE_W L"ActiveTAG Configurator v0.5.10"
-#define ACTIVETAG_WINDOW_TITLE_W L"Zero Density ActiveTAG Configurator v0.5.10"
-#define ACTIVETAG_EXE_NAME_W L"ActiveTAG-Configurator-v0.5.10.exe"
+#define ACTIVETAG_VERSION_MINOR 6
+#define ACTIVETAG_VERSION_PATCH 0
+#define ACTIVETAG_VERSION_W L"v0.6.0"
+#define ACTIVETAG_VERSION_A "v0.6.0"
+#define ACTIVETAG_APP_TITLE_W L"ActiveTAG Configurator v0.6.0"
+#define ACTIVETAG_WINDOW_TITLE_W L"Zero Density ActiveTAG Configurator v0.6.0"
+#define ACTIVETAG_EXE_NAME_W L"ActiveTAG-Configurator-v0.6.0.exe"
